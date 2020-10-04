4b666e7f4e82331b4bc9c4b46a1e33cae8d0c0a5
Salvador Murillo <chavitation@gmail.com>
2020-10-04 12:47:24 -0700

    mobile friendly 90 percent

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+$("#submit").on("click", function () {
+
+    let name = $("#nameInput").val();
+    let email = $("#emailInput").val();
+    let message = $("#messageInput").val();
+
+    console.log(name,email,message);
+
+})
